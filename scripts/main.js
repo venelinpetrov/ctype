c546be7a4281d8467a3c2ef4c80677619b040430
astoilkov <vpetrov33tu@gmail.com>
2016-07-21 16:53:12 +0300

    Implement keyboard navigation.

--- scripts/main.js
+++ scripts/main.js
@@ -4,26 +4,51 @@
 
     function onload() {
         let masterGain = CTX.createGain();
+        let masterGainElem = document.getElementById('master_gain');
         let editorElem = document.querySelector('.editor');
         let kitInputs;
         let currentTypeCallback;
+        let i = 1;
 
         kitInputs = KitsMenu.render(KITS_ARRAY);
 
-        document.getElementById('master_gain').addEventListener('change', e => {
+        masterGainElem.addEventListener('change', e => {
             masterGain.gain.value = e.target.value;
             editorElem.focus();
         });
 
+        document.body.addEventListener('keydown', e => {
+            switch(e.keyCode) {
+                case 9: {
+                    e.preventDefault();
+                    checkRadio(kitInputs[i%kitInputs.length]);
+                    i++;
+                    break;
+                }
+                case 113: {
+                    e.preventDefault();
+                    masterGainElem.value = +masterGainElem.value - .1;
+                    masterGainElem.dispatchEvent(new Event('change'));
+                    break;
+                }
+                case 114: {
+                    e.preventDefault();
+                    masterGainElem.value = +masterGainElem.value + .1;
+                    masterGainElem.dispatchEvent(new Event('change'));
+                    break;
+                }
+            }
+        });
+        
         Array.from(kitInputs, el => {
             el.addEventListener('change', changeKit);
         });
 
-        kitInputs[0].checked = true;
-        kitInputs[0].dispatchEvent(new Event('change'));
+        checkRadio(kitInputs[0]);
 
         function changeKit(e) {
             let kitName = e.target.value;
+
             console.log(kitName)
             Resource.fetchAll.apply(null, window[kitName].kit)
             .then(samples => {
@@ -54,6 +79,12 @@
                 bufferSource = null;
             }
         };
+
+        function checkRadio(radio) {
+            radio.checked = true;
+            radio.dispatchEvent(new Event('change'));
+        }
+
         editorElem.focus();
     };
 })();
